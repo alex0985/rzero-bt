c7c3e07b02ccb9f5b35d80926b9efe52d772daf9
Alex <30656179+alex0985@users.noreply.github.com>
2019-03-15 12:14:07 +0100

    Dist Calc

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ var scanner = new BeaconScanner();
 scanner.onadvertisement = (advertisement) => {
     var beacon = advertisement["iBeacon"];
     beacon.rssi = advertisement["rssi"];
-    console.log(JSON.stringify(beacon, null, "    "));
+
+    var dist = calculateDistance(beacon.rssi);
+    console.log(dist);
+    
+    //console.log(JSON.stringify(beacon, null, "    "));
 };
 
 scanner.startScan().then(() => {
@@ -14,3 +18,18 @@ scanner.startScan().then(() => {
 }).catch((error) => {
     console.error(error);
 });
+
+function calculateDistance(rssi) {
+    var txPower = -59 //hard coded power value. Usually ranges between -59 to -65
+    if (rssi == 0) {
+      return -1.0; 
+    }
+    var ratio = rssi*1.0/txPower;
+    if (ratio < 1.0) {
+      return Math.pow(ratio,10);
+    }
+    else {
+      var distance =  (0.89976)*Math.pow(ratio,7.7095) + 0.111;    
+      return distance;
+    }
+  } 
