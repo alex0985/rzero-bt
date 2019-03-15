5b0f04648673a718dddc2ac703afba1598ad59e6
Alex <30656179+alex0985@users.noreply.github.com>
2019-03-15 10:05:45 +0100

    v0000

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-const noble = require("noble");
+const Noble = require("noble");
 const BeaconScanner = require("node-beacon-scanner");
 
 var scanner = new BeaconScanner();
 
 scanner.onadvertisement = (advertisement) => {
-    //var beacon = advertisement["iBeacon"];
-    console.log(JSON.stringify(advertisement, null, "   "));
-}
+    var beacon = advertisement["iBeacon"];
+    beacon.rssi = advertisement["rssi"];
+    console.log(JSON.stringify(beacon, null, "    "))
+};
 
-scanner.startScan().then(( ) =>{
-    console.log("Scanning started");
-}).catch(error => {
+scanner.startScan().then(() => {
+    console.log("Scanning for BLE devices...")  ;
+}).catch((error) => {
     console.error(error);
 });
